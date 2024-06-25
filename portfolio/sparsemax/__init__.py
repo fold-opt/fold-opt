d581bf11a7c1a47c677a61ea723aa40ddb374d51
J <j@Js-MacBook-Pro.local>
2024-06-25 09:06:13 -0700

    quick fix

--- portfolio/sparsemax/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Top-level package for sparsemax."""
-# fmt: off
-__author__ = """Laksh Aithani"""
-__email__ = "lakshaithanii@gmail.com"
-__version__ = '0.1.8'
-
-from sparsemax.sparsemax import Sparsemax
-# fmt: on
